6ca3d581264950d9b813dd31d306fa922ede39ef
Christian Fischer <sw-dev@computerlyrik.de>
2015-02-27 19:24:12 +0100

    some refactoring and introducing of kitchen

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license           'All rights reserved'
 description       'Installs/Configures sync-1.5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.2.0'
+source_url "https://github.com/computerlyrik/chef-mozilla-firefox-sync.git"
 
 depends 'nginx'
 depends 'certificate'
